ed7337af2e802aef7f6c3dca440eb14d419b1368
Noma <nomathemba.nondzaba@smokeccs.com>
2024-06-26 11:05:28 +0200

    Updated the RegiterStation validation

--- src/test/java/Tests/WeatherApi/WeatherStationTests.java
+++ src/test/java/Tests/WeatherApi/WeatherStationTests.java
@@ -7,16 +7,23 @@ import org.testng.annotations.Test;
 import static Common.BasePaths.WeatherAPI_baseURL;
 import static Common.CommonTestData.Create_Success_Status_Code;
 import static Common.RequestBuilder.registerAWeatherstation;
+import static org.hamcrest.Matchers.notNullValue;
 
 @Test
 @Feature("WeatherStation")
 @Story("Register a new weather station")
 public class WeatherStationTests {
-     public void registerStationTest(){
-          registerAWeatherstation().
-                  then().
-                  assertThat().
-                  statusCode(Create_Success_Status_Code);
-     }
+    public void registerStationTest() {
+        registerAWeatherstation().
+                then().
+                assertThat().
+                statusCode(Create_Success_Status_Code).
+                body("ID", notNullValue()).
+                body("updated_at", notNullValue()).
+                body("created_at", notNullValue()).
+                body("external_id", notNullValue()).
+                body("name", notNullValue()).
+                body("latitude", notNullValue());
+    }
 
 }
